5605a846328f3a61c39368bae5c92e149cb29353
Walter Heck <walter@tribily.com>
2012-02-05 15:05:43 +0200

    fix geppetto warnings (missing or wrongly placed default cases and hyphens in classnames)

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,22 +4,31 @@ class sudo::params {
     RedHat => $::lsbdistcodename ? {
       /^Nahant.*/        => '1.6.7',
       /Tikanga|Santiago/ => '1.7.2',
+      default            => undef
+
     },
     Debian => $::lsbdistcodename ? {
       lenny   => '1.6.9',
       squeeze => '1.7.4',
+      default => undef
     },
     Ubuntu => $::lsbdistcodename ? {
-      lucid => '1.7.4p4-3',
+      lucid    => '1.7.4p4-3',
+      default  => undef
+
     },
     CentOS => $::lsbmajdistrelease ? {
-      5 => '1.7.2',
-      6 => '1.7.2p2',
+      5       => '1.7.2',
+      6       => '1.7.2p2',
+      default => undef
+
     },
+    default   => undef
+
   }
 
-  if !$::sudo_version { 
-    $version = "present" 
+  if !$::sudo_version {
+    $version = "present"
     $majversion = $release_version
   } else {
     $majversion = $::sudo_version
