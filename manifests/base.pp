04025bd1e5a9bbe463878eedea6d7241eda399fd
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2012-04-26 08:22:20 +0200

    sudo: switch from common::concatfilepart to concat[1]
     [1] https://github.com/camptocamp/puppet-concat

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,40 +1,57 @@
 class sudo::base {
-  package {"sudo":
-    ensure => "present",
+
+  package {'sudo':
+    ensure => 'present',
   }
 
-  file {"/etc/sudoers":
-    ensure => present,
-    owner  => root,
-    group  => root,
-    mode   => 440,
-  }
 
   if versioncmp($::sudoversion,'1.7.2') < 0 {
     #
     # Backward compatibility for version less than 1.7.2
-    # 
-    common::concatfilepart { "000-sudoers.init":
-      ensure  => present,
-      manage  => true,
-      file    => "/etc/sudoers",
-      content => template("sudo/sudoers.erb"),
-    }
-   
-  } else {
     #
-    # Use the #includedir directive to manage sudoers.d, version >= 1.7.2
-    # 
-    file {"/etc/sudoers.d":
-      ensure  => directory,
+
+    include concat::setup
+
+    concat {'/etc/sudoers':
       owner   => root,
       group   => root,
-      mode    => 755,
+      mode    => 660,
+    }
+
+    concat::fragment {'000-sudoers.init':
+      ensure  => present,
+      target  => '/etc/sudoers',
+      content => template('sudo/sudoers.erb'),
+    }
+
+    # removed this folder originally created by common::concatfilepart
+    file {'/etc/sudoers.d':
+      ensure  => absent,
       purge   => true,
       recurse => true,
     }
 
-    File ["/etc/sudoers"] { content => template("sudo/sudoers.erb"), }
+  } else {
+    #
+    # Use the #includedir directive to manage sudoers.d, version >= 1.7.2
+    #
+
+    file {'/etc/sudoers':
+      ensure  => present,
+      owner   => root,
+      group   => root,
+      mode    => '0440',
+      content => template('sudo/sudoers.erb'),
+    }
+
+    file {'/etc/sudoers.d':
+      ensure  => directory,
+      owner   => root,
+      group   => root,
+      mode    => '0755',
+      purge   => true,
+      recurse => true,
+    }
 
   }
 
