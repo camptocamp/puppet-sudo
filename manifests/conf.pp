5465ee77c6015eeaba849336308a81e5834ed568
Marc Remy <marc.remy@camptocamp.com>
2013-12-05 16:35:22 +0100

    Set a warning message in sudo::directive

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -12,11 +12,41 @@ define sudo::conf(
   $source = undef,
 ) {
 
-  # Call our sudo::directive with the right parameters
-  sudo::directive{ "${priority}_${name}":
-    ensure  => $ensure,
-    content => $content,
-    source  => $source,
+  # sudo skipping file names that contain a "."
+  $tname = regsubst($name, '\.', '-', 'G')
+  $dname = "${priority}_${tname}"
+
+  if versioncmp($::sudoversion,'1.7.2') >= 0 {
+
+    file {"/etc/sudoers.d/${dname}":
+      ensure  => $ensure,
+      owner   => root,
+      group   => root,
+      mode    => '0440',
+      content => $content ? {
+        ''      => undef,
+        default => $content,
+      },
+      source  => $source ? {
+        ''      => undef,
+        default => $source,
+      },
+      notify => $ensure ? {
+        'present' => Exec["sudo-syntax-check for file ${dname}"],
+        default   => undef,
+      },
+      require => Package['sudo'],
+    }
+
+  } else {
+
+    fail "sudo fragments via #includedir is only available since version 1.7.2 in Sudo[${name}]!"
+
+  }
+
+  exec {"sudo-syntax-check for file ${dname}":
+    command     => "visudo -c -f '/etc/sudoers.d/${dname}' || ( rm -f '/etc/sudoers.d/${dname}' && exit 1)",
+    refreshonly => true,
   }
 
 }
