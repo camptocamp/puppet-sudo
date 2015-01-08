69e99463cde5a6e284b35c5e25cfc22db57a44cb
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-01-08 14:19:22 +0100

    Remove deprecated define sudo::directive

--- manifests/directive.pp
+++
@@ -1,9 +0,0 @@
-define sudo::directive (
-  $ensure  = present,
-  $content = '',
-  $source  = ''
-) {
-
-  fail( "The sudo::directive is deprecated and will be removed soon. Use sudo::conf instead" )
-
-}
